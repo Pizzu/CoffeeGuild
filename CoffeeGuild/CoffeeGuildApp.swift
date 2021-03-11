108f6af357109c69ab106e959895b3ef77871032
Pizzu <lucaloforte3@gmail.com>
2021-03-11 15:54:16 +0100

    First commit.

---
+++ CoffeeGuild/CoffeeGuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeGuildApp.swift
+//  CoffeeGuild
+//
+//  Created by Luca Lo Forte on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeGuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

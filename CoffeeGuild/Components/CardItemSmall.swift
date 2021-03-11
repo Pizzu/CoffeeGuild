f961be1613e76e507578e06302f36cb241def0fd
Pizzu <lucaloforte3@gmail.com>
2021-03-11 17:11:07 +0100

    Create CardItem Small.

---
+++ CoffeeGuild/Components/CardItemSmall.swift
@@ -0,0 +1,68 @@
+//
+//  CardItemSmall.swift
+//  CoffeeGuild
+//
+//  Created by Luca Lo Forte on 11/3/21.
+//
+
+import SwiftUI
+
+struct CardItemSmall: View {
+    var body: some View {
+        HStack(alignment: .center, spacing: 16) {
+            
+            Image("coffee 2")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 75, height: 75)
+                .clipShape(Circle())
+            
+//            Spacer()
+            
+            VStack(alignment: .leading, spacing: 8) {
+                HStack {
+                    Text("Frappucino")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    Text("$8.99")
+                        .font(.footnote)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(#colorLiteral(red: 0.8823529412, green: 0.7098039216, blue: 0.2705882353, alpha: 1)))
+                }
+                
+                HStack {
+                    Text("Milkshake with ice cream, and cappuccino...")
+                        .font(.caption)
+                        .fontWeight(.regular)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Image(systemName: "plus")
+                            .font(.headline)
+                            .foregroundColor(Color(#colorLiteral(red: 0.8823529412, green: 0.7098039216, blue: 0.2705882353, alpha: 1)))
+                            .frame(width: 25, height: 25)
+                    }
+                    
+                    .background(Color(#colorLiteral(red: 0.5019607843, green: 0.3019607843, blue: 0.2156862745, alpha: 1)))
+                    .clipShape(Circle())
+                }
+            }
+            
+        }
+        .padding()
+        .frame(height: 100)
+        .background(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+        .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0.0, y: 10)
+    }
+}
+
+struct CardItemSmall_Previews: PreviewProvider {
+    static var previews: some View {
+        CardItemSmall()
+    }
+}
